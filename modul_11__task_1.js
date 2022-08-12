bbc91262cedb2d4923c6fec6435dff1b77eaf804
Kimi-Oky <109538573+Kimi-Oky@users.noreply.github.com>
2022-08-12 18:46:48 +0300

    Update modul_11__task_1.js

--- modul_11__task_1.js
+++ modul_11__task_1.js
@@ -1,14 +1,21 @@
-let n = prompt ('введите число');
-  x = n++
-if ((typeof n) === "number") {
-    x = x % 2
-    if (x === 0) {
-    console.log ('это чётное число')
-    } else if (x === 1) {
-    console.log ('это нечётное число')
-    }
-} else if ((typeof n) === "nan"){
-   console.log ('вы ввели не число')
-} else {
-  console.log ('Упс, кажется, вы ошиблись')
+let n = [0, 1, 3, 9, 2, 4, 'sine', null];
+let even = 0;
+let odd = 0;
+let zero = 0;
+let others = 0;
+let nullS = 0;
+for (let i = 0; i < n.length; i++){
+  if (n[i] === 0) {
+    zero++;
+  } else if (n[i] % 2 === 0) {
+    even++;
+  } else if (n[i] % 2 === 1) {
+    odd++;
+  } else if (n[i] == null){
+    nullS++; // не разобрался, почему null считает за чётное число
+  } else {
+    others++
+  }
 }
+console.log(`четных: ${even}, нечетных: ${odd}, 
+нолей ${zero}, других: ${others}, элементов null: ${nullS}`)
